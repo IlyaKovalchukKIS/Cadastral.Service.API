ad056f3afbe56953e6366429d7f29c8ce70de7c7
user <mrlivedance@vk.com>
2024-08-08 21:43:01 +0300

    create class DBHelper for open/close async session to database

---
+++ src/repository/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from config import config
+
+engine = create_async_engine(config.DB_URI_async)
+
+
+class DBHelper:
+
+    def __init__(self, uri: str, echo: bool):
+        """
+        Класс помощник для создания асинхронных сессий
+        :param uri: uri базы данных
+        :param echo: выводить ли sql запросы
+        """
+        self.uri = uri
+        self.engine = create_async_engine(url=self.uri, echo=echo)
+        self.__async_session_maker = async_sessionmaker(bind=self.engine, expire_on_commit=False, autoflush=False)
+
+    async def session_dependency(self):
+        """ Создание сессии """
+        async with self.__async_session_maker as session:
+            yield session
+
+
+db_helper = DBHelper(uri=config.DB_URI_async, echo=config.DB_ECHO)
