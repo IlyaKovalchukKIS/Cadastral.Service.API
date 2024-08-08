18ea0cbf1bd74f60b62eef8109cd9f1495043e77
user <mrlivedance@vk.com>
2024-08-08 21:43:27 +0300

    create models Cadastral

---
+++ src/repository/models.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Cadastral(Base):
+    __tablename__ = 'cadastral'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    cadastral_number: Mapped[str]  # кадастровый номер
+    longitude: Mapped[str]  # долгота
+    width: Mapped[str]  # широта
+    date_at: Mapped[datetime] = mapped_column(default=datetime.now())
+    result: Mapped[bool]
