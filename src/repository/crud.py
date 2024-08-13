dd6c3233b56a55111d8ff6248bd6dbcb9cb7c244
user <mrlivedance@vk.com>
2024-08-13 16:14:19 +0300

    add func get_history

--- src/repository/crud.py
+++ src/repository/crud.py
@@ -1,11 +1,17 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.repository.models import Cadaster
 
 
-async def create_cadaster(session, data_in, response_server):
+async def create_cadaster(session, data_in):
     data = data_in.model_dump()
-    data['result'] = response_server
     cadaster = Cadaster(**data)
     session.add(cadaster)
     await session.commit()
     await session.refresh(cadaster)
     return cadaster
+
+
+async def get_history(session: AsyncSession):
+    return await session.execute(select(Cadaster))
