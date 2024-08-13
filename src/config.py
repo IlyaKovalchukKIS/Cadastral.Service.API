23ae2286a547f7c679921cb058b7a6b50787c1f5
user <mrlivedance@vk.com>
2024-08-13 16:14:08 +0300

    change SERVER_URL

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ class Config:
     DB_USER: str = os.getenv("DB_USER")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD")
     DB_ECHO: bool = os.getenv("DB_ECHO")
-    SERVER_URL: str = os.getenv('SERVER_URL')
+    SERVER_URL: str = 'http://server:8001/status/code'
     DB_HOST: str = os.getenv('DB_HOST')
 
     @property
