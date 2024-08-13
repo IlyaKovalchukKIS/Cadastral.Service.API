e28e4034adadf495b0eb975b3f18dc5db81045c9
user <mrlivedance@vk.com>
2024-08-13 22:44:48 +0300

    add docstrings

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,12 @@ import aiohttp
 
 
 async def post_aiohttp(url, data: dict | None = None):
+    """
+    Функция отправки запроса на сервер
+    :param url: url сервера
+    :param data: данные для отправки на сервер
+    :return: ответ сервера
+    """
     async with aiohttp.ClientSession() as session_aiohttp:
         async with session_aiohttp.post(url=url, params=data) as response:
             response_server = await response.json()
