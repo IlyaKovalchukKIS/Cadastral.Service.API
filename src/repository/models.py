ae099732e9e3db1249a8d47caaf84d14fa12646b
user <mrlivedance@vk.com>
2024-08-15 12:56:40 +0300

    Revert "add model User"
    This reverts commit bbfde0e087b221e16ac5e8f901a444eb59988c0a.

--- src/repository/models.py
+++ src/repository/models.py
@@ -1,40 +1,11 @@
 from datetime import datetime
 
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqladmin import ModelView
-from sqlalchemy import text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
-
-
-class User(SQLAlchemyBaseUserTable[int], Base):
-    """
-    id: номер пользователя
-    email: почта пользователя
-    hashed_password: хэш пароля
-    created_at: дата создания пользователя
-    is_active: активен ли пользователь
-    is_superuser: является ли пользователь суперпользователем
-    is_verified: подтвержден ли пользователь
-    cadasters: список проверенных кадастр запросов
-    """
-
-    __tablename__ = "user"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(unique=True, index=True)
-    hashed_password: str
-    created_at: Mapped[datetime] = mapped_column(
-        server_default=text("TIMEZONE('utc', now())")
-    )
-    is_active: bool
-    is_superuser: bool
-    is_verified: bool
-
-    cadasters = relationship("Cadaster", back_populates="user")
 
 
 class Cadaster(Base):
@@ -47,9 +18,6 @@ class Cadaster(Base):
     date_at: Mapped[datetime] = mapped_column(default=datetime.now)
     result: Mapped[bool]
 
-    owner: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    user = relationship("User", back_populates="cadasters")
-
 
 class CadasterAdmin(ModelView, model=Cadaster):
     column_list = [
@@ -58,19 +26,4 @@ class CadasterAdmin(ModelView, model=Cadaster):
         'width',
         'date_at',
         'result',
-        'owner',
-        'user',
-    ]
-
-
-class UserAdmin(ModelView, model=User):
-    column_list = [
-        'id',
-        'email',
-        'hashed_password',
-        'created_at',
-        'is_active',
-        'is_superuser',
-        'is_verified',
-        'cadasters',
     ]
